bb4cb207750c24c70a82009bb18bce6e927b4f8a
Sergey <skiner_1988@mail.ru>
2022-07-25 19:45:15 +0800

    Нехватка памяти

---
+++ F303C_ETH/Debug/Src/subdir.mk
@@ -0,0 +1,81 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/adc.c \
+../Src/dma.c \
+../Src/gpio.c \
+../Src/spi.c \
+../Src/stm32f3xx_hal_msp.c \
+../Src/stm32f3xx_hal_timebase_tim.c \
+../Src/stm32f3xx_it.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/system_stm32f3xx.c \
+../Src/tim.c 
+
+CPP_SRCS += \
+../Src/LED.cpp \
+../Src/filter_sma.cpp \
+../Src/flash_spi.cpp \
+../Src/freertos.cpp \
+../Src/main.cpp \
+../Src/sensor.cpp 
+
+C_DEPS += \
+./Src/adc.d \
+./Src/dma.d \
+./Src/gpio.d \
+./Src/spi.d \
+./Src/stm32f3xx_hal_msp.d \
+./Src/stm32f3xx_hal_timebase_tim.d \
+./Src/stm32f3xx_it.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/system_stm32f3xx.d \
+./Src/tim.d 
+
+OBJS += \
+./Src/LED.o \
+./Src/adc.o \
+./Src/dma.o \
+./Src/filter_sma.o \
+./Src/flash_spi.o \
+./Src/freertos.o \
+./Src/gpio.o \
+./Src/main.o \
+./Src/sensor.o \
+./Src/spi.o \
+./Src/stm32f3xx_hal_msp.o \
+./Src/stm32f3xx_hal_timebase_tim.o \
+./Src/stm32f3xx_it.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/system_stm32f3xx.o \
+./Src/tim.o 
+
+CPP_DEPS += \
+./Src/LED.d \
+./Src/filter_sma.d \
+./Src/flash_spi.d \
+./Src/freertos.d \
+./Src/main.d \
+./Src/sensor.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o Src/%.su: ../Src/%.cpp Src/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m4 -std=gnu++14 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F303xC -c -I../Inc -I../Drivers/STM32F3xx_HAL_Driver/Inc -I../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Drivers/CMSIS/Device/ST/STM32F3xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F303xC -c -I../Inc -I../Drivers/STM32F3xx_HAL_Driver/Inc -I../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Drivers/CMSIS/Device/ST/STM32F3xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/LED.d ./Src/LED.o ./Src/LED.su ./Src/adc.d ./Src/adc.o ./Src/adc.su ./Src/dma.d ./Src/dma.o ./Src/dma.su ./Src/filter_sma.d ./Src/filter_sma.o ./Src/filter_sma.su ./Src/flash_spi.d ./Src/flash_spi.o ./Src/flash_spi.su ./Src/freertos.d ./Src/freertos.o ./Src/freertos.su ./Src/gpio.d ./Src/gpio.o ./Src/gpio.su ./Src/main.d ./Src/main.o ./Src/main.su ./Src/sensor.d ./Src/sensor.o ./Src/sensor.su ./Src/spi.d ./Src/spi.o ./Src/spi.su ./Src/stm32f3xx_hal_msp.d ./Src/stm32f3xx_hal_msp.o ./Src/stm32f3xx_hal_msp.su ./Src/stm32f3xx_hal_timebase_tim.d ./Src/stm32f3xx_hal_timebase_tim.o ./Src/stm32f3xx_hal_timebase_tim.su ./Src/stm32f3xx_it.d ./Src/stm32f3xx_it.o ./Src/stm32f3xx_it.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/system_stm32f3xx.d ./Src/system_stm32f3xx.o ./Src/system_stm32f3xx.su ./Src/tim.d ./Src/tim.o ./Src/tim.su
+
+.PHONY: clean-Src
+
