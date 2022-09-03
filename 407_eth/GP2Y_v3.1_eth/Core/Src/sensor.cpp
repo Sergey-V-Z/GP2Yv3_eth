ededf983889686bb9a2db13881c0ee6a4588e25b
Sergey <skiner_1988@mail.ru>
2022-09-03 19:56:17 +0800

    убрал SMA filter

--- 407_eth/GP2Y_v3.1_eth/Core/Src/sensor.cpp
+++ 407_eth/GP2Y_v3.1_eth/Core/Src/sensor.cpp
@@ -22,11 +22,19 @@ void sensor :: Init(osSemaphoreId *ADC_endHandle, ADC_HandleTypeDef *hadc, uint1
 // обробатываем накопленные данные
 void sensor :: data_processing(uint16_t *data){
 
+	uint32_t Output = 0;
+	/* Sum */
 	for (int var = 0; var < 16; ++var) {
-		Filter_SMA(*data); // обробатываем
+		Output += *data;
 		*data = 0; // обнуляем
 		data++; // идем дальше
 	}
+
+	/* Divide */
+	Result = (uint16_t) (Output / Depth);
+
+	if(Result > peak){peak = Result;}
+	if(Result < gorge){gorge = Result;}
 
 }
 
